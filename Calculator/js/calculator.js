d8ab1b639f92cfcc4f7337b6c4cc7ad7473c40e1
서민정 <79045880+waawa04@users.noreply.github.com>
2021-09-14 08:32:26 +0900

    Add files via upload

---
+++ Calculator/js/calculator.js
@@ -0,0 +1,28 @@
+'use strict';    
+
+let num1="";
+let num2="";
+
+//let operator="";
+
+let op_value = document.querySelector("#operator");
+let result_value = document.querySelector("#result");
+
+document.querySelector("#num-0").addEventListener("click", ()=>{});  //버튼을누르면 어떤 기능을 할지 함수
+document.querySelector("#num-1").addEventListener("click", ()=>{}); 
+document.querySelector("#num-2").addEventListener("click", ()=>{}); 
+document.querySelector("#num-3").addEventListener("click", ()=>{}); 
+document.querySelector("#num-4").addEventListener("click", ()=>{}); 
+document.querySelector("#num-5").addEventListener("click", ()=>{}); 
+document.querySelector("#num-6").addEventListener("click", ()=>{}); 
+document.querySelector("#num-7").addEventListener("click", ()=>{}); 
+document.querySelector("#num-8").addEventListener("click", ()=>{}); 
+document.querySelector("#num-9").addEventListener("click", ()=>{}); 
+document.querySelector("#plus").addEventListener("click", ()=>{}); 
+document.querySelector("#subtract").addEventListener("click", ()=>{});
+document.querySelector("#multiply").addEventListener("click", ()=>{});  
+document.querySelector("#divide").addEventListener("click", ()=>{}); 
+document.querySelector("#clear").addEventListener("click", ()=>{}); 
+document.querySelector("#calculate").addEventListener("click", ()=>{}); 
+
+
